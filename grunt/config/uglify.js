0169777f61f0b426c0b806b89f7289f555b6ef17
Phat Pham <phatpham9@gmail.com>
2016-11-15 22:29:38 +0700

    Update grunt uglify task config

--- grunt/config/uglify.js
+++ grunt/config/uglify.js
@@ -8,7 +8,6 @@
 module.exports = function(grunt) {
     grunt.config.set('uglify', {
         options: {
-            expression: true,
             preserveComments: false
         },
         public: {
